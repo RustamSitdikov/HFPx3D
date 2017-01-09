f5840a738148f2a0356d7424ced4a4588a6e9258
nikolski <dmitry.nikolskiy@epfl.ch>
2017-01-09 18:21:34 +0100

    added assembly of element-to-point influence (sub-matrix) using il::blas function

--- ICFns.h
+++ ICFns.h
@@ -2,7 +2,8 @@
 // This file is part of 3d_bem.
 //
 // Created by nikolski on 1/7/2017.
-// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+// Geo-Energy Laboratory, 2016-2017.  All rights reserved.
 // See the LICENSE.TXT file for more details. 
 //
 
