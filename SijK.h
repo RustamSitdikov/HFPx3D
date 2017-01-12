3bdadca3ebb0c4a9ab7fb08197701b9ed3bc2cee
nikolski <dmitry.nikolskiy@epfl.ch>
2017-01-12 23:21:35 +0100

    introduced Kernel_Int class to be able to assemble influence of any kernel (T or H)

---
+++ SijK.h
@@ -0,0 +1,27 @@
+//
+// This file is part of 3d_bem.
+//
+// Created by nikolski on 1/12/2017.
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// See the LICENSE.TXT file for more details. 
+//
+
+#ifndef INC_3D_BEM_SIJK_H
+#define INC_3D_BEM_SIJK_H
+
+#endif //INC_3D_BEM_SIJK_H
+
+class Kernel_Int {
+public:
+    il::StaticArray3D<std::complex<double>, 6, 3, 9> S11_22(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 5> S11_22_red(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 9> S11_22_12(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 5> S11_22_12_red(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 9> S13_23(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 5> S13_23_red(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 9> S33(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 3, 5> S33_red(double nu, std::complex<double> eix, double h, std::complex<double> d);
+    il::StaticArray3D<std::complex<double>, 6, 4, 3> SijLim(double nu, std::complex<double> eix, std::complex<double> d);
+};
+
+// Kernel_Int H_Int; //, T_Int;
