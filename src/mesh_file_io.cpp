6021f3cc9c39e7e3c0293e2a613ceeaa7eb326fd
nikolski <dmitry.nikolskiy@epfl.ch>
2017-06-28 22:35:36 +0200

    added il::String support and .csv file input (mesh)

--- src/mesh_file_io.cpp
+++ src/mesh_file_io.cpp
@@ -7,15 +7,16 @@
 // See the LICENSE.TXT file for more details. 
 //
 
-//#include <cstdio>
+#include <cstdio>
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <sstream>
 #include <il/Array.h>
 #include <il/Array2D.h>
 #include <il/StaticArray.h>
 #include <il/StaticArray2D.h>
 #include <il/io/numpy/numpy.h>
-#include <fstream>
 #include "mesh_file_io.h"
 
 namespace hfp3d {
@@ -30,10 +31,11 @@ namespace hfp3d {
 // (32-bit integer)
 // and node coordinates matrix (3*N_nodes) from numpy binary files
         il::Status status{};
-        std::string f_path = src_dir + conn_f_name;
+        std::string c_f_path = src_dir + conn_f_name;
+        std::string n_f_path = src_dir + node_f_name;
 
         il::Array2D<int> mesh_conn_tmp = il::load<il::Array2D<int>>
-                (f_path, il::io, status);
+                (c_f_path, il::io, status);
         status.abortOnError();
         mesh.conn.resize(mesh_conn_tmp.size(0), mesh_conn_tmp.size(1));
         for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
@@ -43,7 +45,49 @@ namespace hfp3d {
         }
 
         mesh.nods = il::load<il::Array2D<double>>
-                (src_dir + node_f_name, il::io, status);
+                (n_f_path, il::io, status);
+        status.abortOnError();
+
+        if (is_matlab) {
+            // conversion from Matlab (array numbering starting with 1)
+            // to C++ standard (array numbering starting with 0)
+            il::int_t n_r = (mesh.conn.size(0) >= 3) ? 3 : mesh.conn.size(0);
+            for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
+                for (il::int_t j = 0; j < n_r; ++j) {
+                    mesh.conn(j, k) -=1;
+                }
+            }
+        }
+    }
+
+    // overloaded for il::String
+    void load_mesh_from_numpy_32
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh) {
+// This function reads the mesh connectivity matrix (3*N_elements)
+// (32-bit integer)
+// and node coordinates matrix (3*N_nodes) from numpy binary files
+        il::Status status{};
+        il::String c_f_path(src_dir); c_f_path.append("/");
+        c_f_path.append(conn_f_name);
+        il::String n_f_path(src_dir); n_f_path.append("/");
+        n_f_path.append(node_f_name);
+
+        il::Array2D<int> mesh_conn_tmp = il::load<il::Array2D<int>>
+                (c_f_path, il::io, status);
+        status.abortOnError();
+        mesh.conn.resize(mesh_conn_tmp.size(0), mesh_conn_tmp.size(1));
+        for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
+            for (il::int_t j = 0; j < mesh.conn.size(0); ++j) {
+                mesh.conn(j, k) = mesh_conn_tmp(j, k);
+            }
+        }
+
+        mesh.nods = il::load<il::Array2D<double>>
+                (n_f_path, il::io, status);
         status.abortOnError();
 
         if (is_matlab) {
@@ -68,14 +112,49 @@ namespace hfp3d {
 // (64-bit integer)
 // and node coordinates matrix (3*N_nodes) from numpy binary files
         il::Status status{};
-        std::string f_path = src_dir + conn_f_name;
+        std::string c_f_path = src_dir + conn_f_name;
+        std::string n_f_path = src_dir + node_f_name;
 
         mesh.conn = il::load<il::Array2D<il::int_t>>
-                (f_path, il::io, status);
+                (c_f_path, il::io, status);
         status.abortOnError();
 
         mesh.nods = il::load<il::Array2D<double>>
-                (src_dir + node_f_name, il::io, status);
+                (n_f_path, il::io, status);
+        status.abortOnError();
+
+        if (is_matlab) {
+            // conversion from Matlab (array numbering starting with 1)
+            // to C++ standard (array numbering starting with 0)
+            il::int_t n_r = (mesh.conn.size(0) >= 3) ? 3 : mesh.conn.size(0);
+            for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
+                for (il::int_t j = 0; j < n_r; ++j) {
+                    mesh.conn(j, k) -=1;
+                }
+            }
+        }
+    }
+
+    // overloaded for il::String
+    void load_mesh_from_numpy_64
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh) {
+// This function reads the mesh connectivity matrix (3*N_elements)
+// (64-bit integer)
+// and node coordinates matrix (3*N_nodes) from numpy binary files
+        il::Status status{};
+        il::String c_f_path(src_dir); c_f_path.append(conn_f_name);
+        il::String n_f_path(src_dir); n_f_path.append(node_f_name);
+
+        mesh.conn = il::load<il::Array2D<il::int_t>>
+                (c_f_path, il::io, status);
+        status.abortOnError();
+
+        mesh.nods = il::load<il::Array2D<double>>
+                (n_f_path, il::io, status);
         status.abortOnError();
 
         if (is_matlab) {
@@ -99,30 +178,53 @@ namespace hfp3d {
 // This function reads the mesh connectivity matrix (3*N_elements)
 // and node coordinates matrix (3*N_nodes) from CSV files.
 //
-        std::string f_path = src_dir + conn_f_name;
-        std::string line;
-        const char* format = "%d,";
-        std::ifstream cf;
-        cf.open(f_path.c_str());
+        std::string c_f_path = src_dir + conn_f_name;
+        std::string n_f_path = src_dir + node_f_name;
+
+        std::string line = "", subline = "";
+        char delim = ',';
+
+        std::ifstream cf; // connectivity file stream
+        cf.open(c_f_path.c_str());
         if(cf.is_open()) {
+            // counting the array size
             il::int_t n_row = 0, n_col = 0;
             while(!cf.eof()) {
                 std::getline(cf, line);
-                ++n_row;
-                il::int_t n_r_t = 0, pos = 0;
-                while((pos = line.find(",", pos)) < line.length()) {
-                    ++n_r_t;
+                if (line.length() == 0) {
+                    std::cout << "Empty row" << std::endl;
+                } else {
+                    std::stringstream linestream(line);
+                    ++n_row;
+                    il::int_t n_col_t = 0;
+                    do {
+                        subline = "";
+                        std::getline(linestream, subline, delim);
+                        if (subline.length() > 0){
+                            ++n_col_t;
+                        }
+                    } while(subline.length() > 0);
+                    if (n_col_t != n_col && n_col > 0) {
+                        std::cout << "Row size is not constant" << std::endl;
+                    }
+                    else n_col = n_col_t;
                 }
-                if (n_r_t + 1 != n_col && n_col > 0) {
-                    std::cout << "Row size is not constant" << std::endl;
-                }
-                else n_col = n_r_t + 1;
             }
+            // resizing the output array (mesh connectivity)
             mesh.conn.resize(n_col, n_row);
+            // importing the array
             for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
                 std::getline(cf, line);
+                std::stringstream linestream(line);
                 for (il::int_t j = 0; j < mesh.conn.size(0); ++j) {
-                    std::sscanf(line.c_str(), format, mesh.conn(j, k));
+                    subline = "";
+                    std::getline(linestream, subline, delim);
+                    if (subline.length() > 0){
+                        il::int_t value = -1;
+                        std::stringstream sublinestream(subline);
+                        sublinestream >> value;
+                        mesh.conn(j, k) = value;
+                    }
                 }
             }
             if (is_matlab) {
@@ -135,36 +237,181 @@ namespace hfp3d {
                     }
                 }
             }
+            cf.close();
         }
         else {
             std::cout << "Can't open the file" << std::endl;
         }
 
-        f_path = src_dir + node_f_name;
-        format = "%g,";
-        std::ifstream nf;
-        nf.open(f_path.c_str());
+        std::ifstream nf; // node coordinates file stream
+        nf.open(n_f_path.c_str());
         if(nf.is_open()) {
+            // counting the array size
             il::int_t n_row = 0, n_col = 0;
             while(!nf.eof()) {
                 std::getline(nf, line);
-                ++n_row;
-                il::int_t n_r_t = 0, pos = 0;
-                while((pos = line.find(",", pos)) < line.length()) {
-                    ++n_r_t;
+                if (line.length() == 0) {
+                    std::cout << "Empty row" << std::endl;
+                } else {
+                    std::stringstream linestream(line);
+                    ++n_row;
+                    il::int_t n_col_t = 0;
+                    do {
+                        subline = "";
+                        std::getline(linestream, subline, delim);
+                        if (subline.length() > 0){
+                            ++n_col_t;
+                        }
+                    } while(subline.length() > 0);
+                    if (n_col_t != n_col && n_col > 0) {
+                        std::cout << "Row size is not constant" << std::endl;
+                    }
+                    else n_col = n_col_t;
                 }
-                if (n_r_t + 1 != n_col && n_col > 0) {
-                    std::cout << "Row size is not constant" << std::endl;
-                }
-                else n_col = n_r_t + 1;
             }
+            // resizing the output array (node coordinates)
             mesh.nods.resize(n_col, n_row);
+            // importing the array
             for (il::int_t k = 0; k < mesh.nods.size(1); ++k) {
                 std::getline(nf, line);
+                std::stringstream linestream(line);
                 for (il::int_t j = 0; j < mesh.nods.size(0); ++j) {
-                    std::sscanf(line.c_str(), format, mesh.nods(j, k));
+                    subline = "";
+                    std::getline(linestream, subline, delim);
+                    if (subline.length() > 0){
+                        double value = -1.0;
+                        std::stringstream sublinestream(subline);
+                        sublinestream >> value;
+                        mesh.nods(j, k) = value;
+                    }
                 }
             }
+            nf.close();
+        }
+        else {
+            std::cout << "Can't open the file" << std::endl;
+        }
+    }
+
+    // overloaded for il::String
+    void load_mesh_from_csv
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh) {
+// This function reads the mesh connectivity matrix (3*N_elements)
+// and node coordinates matrix (3*N_nodes) from CSV files.
+//
+        il::String c_f_path(src_dir); c_f_path.append(conn_f_name);
+        il::String n_f_path(src_dir); n_f_path.append(node_f_name);
+
+        std::string line = "", subline = "";
+        char delim = ',';
+
+        std::ifstream cf; // connectivity file stream
+        cf.open(c_f_path.asCString());
+        if(cf.is_open()) {
+            // counting the array size
+            il::int_t n_row = 0, n_col = 0;
+            while(!cf.eof()) {
+                std::getline(cf, line);
+                if (line.length() == 0) {
+                    std::cout << "Empty row" << std::endl;
+                } else {
+                    std::stringstream linestream(line);
+                    ++n_row;
+                    il::int_t n_col_t = 0;
+                    do {
+                        subline = "";
+                        std::getline(linestream, subline, delim);
+                        if (subline.length() > 0){
+                            ++n_col_t;
+                        }
+                    } while(subline.length() > 0);
+                    if (n_col_t != n_col && n_col > 0) {
+                        std::cout << "Row size is not constant" << std::endl;
+                    }
+                    else n_col = n_col_t;
+                }
+            }
+            // resizing the output array (mesh connectivity)
+            mesh.conn.resize(n_col, n_row);
+            // importing the array
+            for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
+                std::getline(cf, line);
+                std::stringstream linestream(line);
+                for (il::int_t j = 0; j < mesh.conn.size(0); ++j) {
+                    subline = "";
+                    std::getline(linestream, subline, delim);
+                    if (subline.length() > 0){
+                        il::int_t value = -1;
+                        std::stringstream sublinestream(subline);
+                        sublinestream >> value;
+                        mesh.conn(j, k) = value;
+                    }
+                }
+            }
+            if (is_matlab) {
+                // conversion from Matlab (array numbering starting with 1)
+                // to C++ standard (array numbering starting with 0)
+                il::int_t n_r = (mesh.conn.size(0) >= 3) ? 3 : mesh.conn.size(0);
+                for (il::int_t k = 0; k < mesh.conn.size(1); ++k) {
+                    for (il::int_t j = 0; j < n_r; ++j) {
+                        mesh.conn(j, k) -= 1;
+                    }
+                }
+            }
+            cf.close();
+        }
+        else {
+            std::cout << "Can't open the file" << std::endl;
+        }
+
+        std::ifstream nf; // node coordinate file stream
+        nf.open(n_f_path.asCString());
+        if(nf.is_open()) {
+            // counting the array size
+            il::int_t n_row = 0, n_col = 0;
+            while(!nf.eof()) {
+                std::getline(nf, line);
+                if (line.length() == 0) {
+                    std::cout << "Empty row" << std::endl;
+                } else {
+                    std::stringstream linestream(line);
+                    ++n_row;
+                    il::int_t n_col_t = 0;
+                    do {
+                        subline = "";
+                        std::getline(linestream, subline, delim);
+                        if (subline.length() > 0){
+                            ++n_col_t;
+                        }
+                    } while(subline.length() > 0);
+                    if (n_col_t != n_col && n_col > 0) {
+                        std::cout << "Row size is not constant" << std::endl;
+                    }
+                    else n_col = n_col_t;
+                }
+            }
+            // resizing the output array (node coordinates)
+            mesh.nods.resize(n_col, n_row);
+            // importing the array
+            for (il::int_t k = 0; k < mesh.nods.size(1); ++k) {
+                std::getline(nf, line);
+                std::stringstream linestream(line);
+                for (il::int_t j = 0; j < mesh.nods.size(0); ++j) {
+                    subline = "";
+                    std::getline(linestream, subline, delim);
+                    if (subline.length() > 0){
+                        double value = -1.0;
+                        std::stringstream sublinestream(subline);
+                        sublinestream >> value;
+                        mesh.nods(j, k) = value;
+                    }
+                }
+            }
+            nf.close();
         }
         else {
             std::cout << "Can't open the file" << std::endl;
--- src/mesh_file_io.h
+++ src/mesh_file_io.h
@@ -11,13 +11,14 @@
 #define INC_HFPX3D_MESH_FILE_IO_H
 
 #include <cstdio>
-//#include <iostream>
+#include <iostream>
+#include <fstream>
 #include <complex>
 #include <il/Array.h>
 #include <il/Array2D.h>
 #include <il/StaticArray.h>
 #include <il/StaticArray2D.h>
-#include "mesh_utilities.h"
+#include "surface_mesh_utilities.h"
 
 namespace hfp3d {
 
@@ -28,6 +29,14 @@ namespace hfp3d {
              bool is_matlab,
              il::io_t, Mesh_Geom_T &mesh);
 
+    // overload for il::String
+    void load_mesh_from_numpy_32
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh);
+
     void load_mesh_from_numpy_64
             (const std::string &src_dir,
              const std::string &conn_f_name,
@@ -35,10 +44,26 @@ namespace hfp3d {
              bool is_matlab,
              il::io_t, Mesh_Geom_T &mesh);
 
+    // overload for il::String
+    void load_mesh_from_numpy_64
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh);
+
     void load_mesh_from_csv
             (const std::string &src_dir,
              const std::string &conn_f_name,
              const std::string &node_f_name,
+             bool is_matlab,
+             il::io_t, Mesh_Geom_T &mesh);
+
+    // overload for il::String
+    void load_mesh_from_csv
+            (const il::String &src_dir,
+             const il::String &conn_f_name,
+             const il::String &node_f_name,
              bool is_matlab,
              il::io_t, Mesh_Geom_T &mesh);
 
@@ -81,6 +106,28 @@ namespace hfp3d {
         std::fclose(of);
     }
 
+    // overload for il::String
+    template <typename T, il::int_t m>
+    void save_data_to_csv(
+            const il::StaticArray<T, m> &vector,
+            const il::String &trg_dir,
+            const il::String &of_name) {
+        il::String f_path = trg_dir; f_path.append(of_name);
+        const char* f_p_c = f_path.asCString();
+        const char* format;
+        if (sizeof(T) == sizeof(std::complex<double>)) {
+            format = "%.16g%+.16gi\n";
+        } else {
+            format = "%.16g\n";
+        }
+        FILE* of=std::fopen(f_p_c,"w");
+        for (int j=0; j < vector.size(); ++j){
+            T out = vector[j];
+            std::fprintf(of, format, out);
+        }
+        std::fclose(of);
+    }
+
     template <typename T>
     void save_data_to_csv(
             const il::Array2D<T> &matrix,
@@ -105,6 +152,39 @@ namespace hfp3d {
         std::fclose(of);
     }
 
+    // overload for il::String
+    template <typename T>
+    void save_data_to_csv(
+            const il::Array2D<T> &matrix,
+            const il::String &trg_dir,
+            const il::String &of_name) {
+        il::String f_path = trg_dir; f_path.append("/");
+        f_path.append(of_name);
+        const char* f_p_c = f_path.asCString();
+        const char* format;
+        if (sizeof(T) == sizeof(std::complex<double>)) {
+            format = "%.16g%+.16gi";
+        } else {
+            format = "%.16g";
+        }
+        std::ofstream ofs(f_p_c);
+        if (ofs.is_open()) {
+            for (int j=0; j < matrix.size(0); ++j) {
+                for (int k=0; k < matrix.size(1); ++k) {
+                    T out = matrix(j, k);
+                    ofs << out;
+                    if (k < matrix.size(1) - 1) {
+                        ofs << ",";
+                    }
+                }
+                ofs << std::endl;
+            }
+            ofs.close();
+        } else {
+            std::cout << "Can't open the file" << std::endl;
+        }
+    }
+
     template <class T, il::int_t m, il::int_t n>
     void save_data_to_csv(
             const il::StaticArray2D<T, m, n> &matrix,
@@ -118,6 +198,32 @@ namespace hfp3d {
             format = "%.16g";
         }
         FILE* of=std::fopen(f_path.c_str(),"w");
+        for (int j=0; j < matrix.size(0); ++j) {
+            for (int k=0; k < matrix.size(1); ++k) {
+                T out = matrix(j, k);
+                std::fprintf(of, format, out);
+                if (k < matrix.size(1)-1) std::fprintf(of, ",");
+            }
+            std::fprintf(of, "\n");
+        }
+        std::fclose(of);
+    }
+
+    // overload for il::String
+    template <class T, il::int_t m, il::int_t n>
+    void save_data_to_csv(
+            const il::StaticArray2D<T, m, n> &matrix,
+            const il::String &trg_dir,
+            const il::String &of_name) {
+        il::String f_path = trg_dir; f_path.append(of_name);
+        const char* f_p_c = f_path.asCString();
+        const char* format;
+        if (sizeof(T) == sizeof(std::complex<double>)) {
+            format = "%.16g%+.16gi";
+        } else {
+            format = "%.16g";
+        }
+        FILE* of=std::fopen(f_p_c,"w");
         for (int j=0; j < matrix.size(0); ++j) {
             for (int k=0; k < matrix.size(1); ++k) {
                 T out = matrix(j, k);
